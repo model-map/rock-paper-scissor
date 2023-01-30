a4fb078237324a19df59fcdf4990d91a1afb7e28
J <118442144+model-map@users.noreply.github.com>
2023-01-30 20:29:08 +0530

    Make rock float randomly in a box

--- script.js
+++ script.js
@@ -1,75 +1,21 @@
-/* 
-Function list:
-    function getPlayerChoice();
-    function getComputerChoice();
-    function playRound();
-    function game();
-*/
+const container=document.querySelector('.container');
 
-function getPlayerChoice(){ // Get player's choice
-    let playerChoice=prompt('Enter your choice, fighter! [rock, paper, scissor]','rock');
-    playerChoice=playerChoice.toLowerCase().trim();
-    if (!['rock','paper','scissors'].includes(playerChoice)) getPlayerChoice();
-    return playerChoice;
-}
+container.addEventListener('click',(e)=>{
+    console.log(`X: ${e.clientX}`);
+    console.log(`Y: ${e.clientY}`);
+})
 
-function getComputerChoice(){
-    const choice_list=['rock','paper','scissors'];
-    let random_index=Math.floor(Math.random()*3); //Generate a random number from 0 to 2 for index.
-    let computerChoice=choice_list[random_index];
-    return (computerChoice);
-}
+// X -> 290 to 1630
+// Y -> 200 to 480
 
-function playRound(playerChoice){
-    // Function returns player's result.
-    // 0 if player lost
-    // 1 if player won
-    // null if it's a draw
-    const choiceIndexPair={'rock':0,'paper':1,'scissors':2};
-    let computerChoice=getComputerChoice();
-    let playerChoiceIndex=choiceIndexPair[playerChoice];
-    let computerChoiceIndex=choiceIndexPair[computerChoice];
-    let choiceIndexPairLength= Object.keys(choiceIndexPair).length;
-    let result='draw';
+const floatingParticipant = document.querySelectorAll('.participant');
 
-/*     console.log(`Players choice ${playerChoice} and index ${playerChoiceIndex}\nComputer choice ${computerChoice} and index ${computerChoiceIndex}`); */
-
-    if (playerChoiceIndex==computerChoiceIndex){
-    }
-    else if ((playerChoiceIndex!=(choiceIndexPairLength-1))&&(computerChoiceIndex==playerChoiceIndex+1)){
-        result='loss';
-    }
-    else if ((playerChoiceIndex==(choiceIndexPairLength-1))&&(computerChoiceIndex==0)){
-        result='loss';
-    }
-    else{
-        result='win';
-    }
-    return (result);
-}
-
-function game(){
-    let total_rounds=parseInt(prompt("Enter the number of rounds you'll fight for, fighter!",100));
-    let win,loss,draw;
-    win=loss=draw=0;
-    let playerChoice=getPlayerChoice();
-
-    for (i=1;i<=total_rounds;i++){
-        let result=playRound(playerChoice);
-        
-        switch(result){
-            case 'loss':
-                loss++;
-                break;
-            case 'win':
-                win++;
-                break;
-            default:
-                draw++;
-                break;
-        }
-    }
-
-    return (`\nOut of ${total_rounds} rounds you played with ${playerChoice}\nYou Won: ${win}\nLost: ${loss}\nand Drew: ${draw} `);
-    
-}
+setInterval(()=>{
+    const X=Math.floor(Math.random()*(1630-290+1)+290);
+    const Y=Math.floor(Math.random()*(480-200+1)+200);
+    floatingParticipant.forEach((participant)=>{
+        console.log(participant);
+        participant.style.left=`${X}px`;
+        participant.style.top=`${Y}px`;
+    })
+},1);
