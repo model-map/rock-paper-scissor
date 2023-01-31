0ced8ccb5718914f6280efe72afe84767ce4e33a
J <118442144+model-map@users.noreply.github.com>
2023-01-31 08:04:22 +0530

    Set setInterval, transition delay from 1ms to 1s

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const container=document.querySelector('.container');
+/* const container=document.querySelector('.container');
 
 container.addEventListener('click',(e)=>{
     console.log(`X: ${e.clientX}`);
     console.log(`Y: ${e.clientY}`);
-})
+}) */
 
 // X -> 290 to 1630
 // Y -> 200 to 480
@@ -11,11 +11,11 @@ container.addEventListener('click',(e)=>{
 const floatingParticipant = document.querySelectorAll('.participant');
 
 setInterval(()=>{
-    const X=Math.floor(Math.random()*(1630-290+1)+290);
-    const Y=Math.floor(Math.random()*(480-200+1)+200);
     floatingParticipant.forEach((participant)=>{
+        const X=Math.floor(Math.random()*(1630-290)+290);
+        const Y=Math.floor(Math.random()*(480-200+1)+200);
         console.log(participant);
         participant.style.left=`${X}px`;
         participant.style.top=`${Y}px`;
-    })
-},1);
+    });
+},1000);
